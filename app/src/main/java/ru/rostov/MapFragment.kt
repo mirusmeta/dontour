6ffb7282b936d4743ab4f0cfef808aa1ff024f38
Keshula <infodevmirus@gmail.com>
2026-02-26 19:25:15 +0300

    2.42

--- app/src/main/java/ru/rostov/MapFragment.kt
+++ app/src/main/java/ru/rostov/MapFragment.kt
@@ -1,62 +1,30 @@
 package ru.rostov
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.graphics.PointF
-import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.OvershootInterpolator
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.RequestPoint
-import com.yandex.mapkit.RequestPointType
-import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.DrivingRoute
-import com.yandex.mapkit.directions.driving.DrivingRouter
-import com.yandex.mapkit.directions.driving.DrivingSession
-import com.yandex.mapkit.directions.driving.DrivingOptions
-import com.yandex.mapkit.directions.driving.DrivingRouterType
-import com.yandex.mapkit.directions.driving.VehicleOptions
-import com.yandex.mapkit.directions.driving.VehicleType
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraListener
-import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapType
+import com.yandex.mapkit.map.VisibleRegionUtils
 import com.yandex.mapkit.mapview.MapView
-import com.yandex.mapkit.transport.TransportFactory
-import com.yandex.mapkit.transport.masstransit.PedestrianRouter
-import com.yandex.mapkit.transport.masstransit.Route
-import com.yandex.mapkit.transport.masstransit.RouteOptions
-import com.yandex.mapkit.transport.masstransit.Session
-import com.yandex.mapkit.transport.masstransit.TimeOptions
+import com.yandex.mapkit.search.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.Locale
-import kotlin.collections.first
-import kotlin.collections.isNotEmpty
-import androidx.core.graphics.toColorInt
-import com.yandex.mapkit.map.CameraUpdateReason
-import com.yandex.mapkit.map.Map
-import com.yandex.mapkit.map.MapType
-import com.yandex.mapkit.transport.masstransit.FitnessOptions
-import com.yandex.runtime.Error
-import com.yandex.runtime.image.ImageProvider
+import ru.rostov.LiveParser.Place
+import ru.rostov.LiveParser.PlacesParser
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MapFragment : Fragment() {
 
     private lateinit var mapView: MapView
+    private lateinit var searchManager: SearchManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,15 +32,90 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+
         mapView = view.findViewById(R.id.mapview)
+
+        // тёмная карта
         mapView.map.mapType = MapType.VECTOR_MAP
         mapView.map.isNightModeEnabled = true
 
+        // стартовая позиция Ростов
         mapView.map.move(
-            CameraPosition(Point(47.2357, 39.7015), 12f, 0f, 0f)
+            com.yandex.mapkit.map.CameraPosition(
+                Point(47.2357, 39.7015),
+                12f,
+                0f,
+                0f
+            )
         )
 
+        searchManager = SearchFactory.getInstance()
+            .createSearchManager(SearchManagerType.COMBINED)
+
+        loadPlaces()
+
         return view
+    }
+
+    private fun loadPlaces() {
+        lifecycleScope.launch {
+            val places = withContext(Dispatchers.IO) {
+                PlacesParser(requireContext()).parsePlaces()
+            }
+
+            addPlacesToMap(places)
+        }
+    }
+
+    private suspend fun addPlacesToMap(places: List<Place>) {
+        for (place in places) {
+            val point = geocode(place.adress)
+            if (point != null) {
+                addPlacemark(point, place)
+            }
+            delay(120) // чтобы не ловить лимиты геокода
+        }
+    }
+
+    private suspend fun geocode(address: String): Point? =
+        withContext(Dispatchers.Main) {
+            suspendCoroutine { cont ->
+
+                val options = SearchOptions().apply {
+                    searchTypes = SearchType.GEO.value
+                }
+
+                searchManager.submit(
+                    address,
+                    VisibleRegionUtils.toPolygon(mapView.map.visibleRegion),
+                    options,
+                    object : Session.SearchListener {
+
+                        override fun onSearchResponse(response: Response) {
+                            val obj = response.collection.children.firstOrNull()
+                            val point = obj?.obj?.geometry?.firstOrNull()?.point
+                            cont.resume(point)
+                        }
+
+                        override fun onSearchError(error: com.yandex.runtime.Error) {
+                            cont.resume(null)
+                        }
+                    }
+                )
+            }
+        }
+
+    private fun addPlacemark(point: Point, place: Place) {
+        val placemark = mapView.map.mapObjects.addPlacemark(point)
+
+        // дефолтная метка MapKit (без setIcon)
+
+        placemark.userData = place
+
+        placemark.addTapListener { _, _ ->
+            // TODO открыть карточку объекта
+            true
+        }
     }
 
     override fun onStart() {
